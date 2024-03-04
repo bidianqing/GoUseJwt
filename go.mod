532b53b0acd6add8c7eeee327b7fea6ddc63b78b
bidianqing <bidianqing@qq.com>
2024-03-04 22:33:45 +0800

    *

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/bidianqing/go-use-jwt
 
 go 1.22.0
 
-require github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+)
--- main.go
+++ main.go
@@ -2,15 +2,36 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
+/*
+iss（全称为 issuer），指明 JWT 是由谁签发的
+sub（全称为 subject），指明 JWT 的主题（也可理解为面向用户的类型）
+aud（全称为 audience），指明 JWT 希望谁签收
+exp（全称为 expiration time），指明 JWT 的过期时间，过期时间需大于签发时间
+nbf（全称为 not before time），指明 JWT 在哪个时间点生效
+iat（全称为 issued at time），指明 JWT 的签发时间
+jti（全称为 JWT ID），指明 JWT 唯一 ID，用于避免重放攻击
+//*/
+
 func main() {
+	key := "hellojwthellocsharpbidianqingbidianqing"
+
 	t := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"iss":  "bidianqing",
+		"aud":  "golang",
+		"sub":  "auth",
+		"exp":  jwt.NewNumericDate(time.Now().Add(1 * time.Minute)),
+		"nbf":  jwt.NewNumericDate(time.Now()),
+		"iat":  jwt.NewNumericDate(time.Now()),
+		"jti":  uuid.New(),
 		"name": "1",
 	})
-	token, err := t.SignedString([]byte("hellogolang"))
+	token, err := t.SignedString([]byte(key))
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -19,12 +40,12 @@ func main() {
 	fmt.Println(token)
 
 	decodeToken, err := jwt.ParseWithClaims(token, jwt.MapClaims{}, func(t *jwt.Token) (interface{}, error) {
-		return []byte("hellogolang"), nil
+		return []byte(key), nil
 	})
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	fmt.Println(decodeToken.Claims.(jwt.MapClaims)["name"])
+	fmt.Println(decodeToken.Claims.(jwt.MapClaims))
 }
